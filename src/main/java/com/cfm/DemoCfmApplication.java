c8b2e27344bcf2950cd0e48791fc1e0c9673af34
cfm <1454912433@qq.com>
2018-10-11 21:22:11 +0800

    chaifeimanshizhu

---
+++ src/main/java/com/cfm/DemoCfmApplication.java
@@ -0,0 +1,12 @@
+package com.cfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCfmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCfmApplication.class, args);
+    }
+}
---
+++ src/main/java/com/cfm/controller.java
@@ -0,0 +1,12 @@
+package com.cfm;
+
+public class controller {
+
+    public static void sys(){
+        System.out.println("张美丽");
+        System.out.println("欧阳傻逼");
+        System.out.println("欧阳11111傻逼");
+        System.out.println("欧阳22222傻逼111122");
+        System.out.println("145464d6");
+    }
+}
